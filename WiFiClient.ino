17a47490699c521b293cd6eb99d3d3e0e592b5e2
Samuel Skanberg <samuel.skanberg@gmail.com>
2020-11-21 17:17:33 +0100

    Change license from UNLICENSE to Apache 2.0
    Why? A friend told me that UNLICENSE might create a headache for legal reasons.
    
    So better to choose a recognized permissive license such as MIT, BSD or Apache.
    
    Why Apache? Because of patents! And FSF recommends it for smaller programs
    
    Read more:
    
    https://opensource.com/article/18/2/how-make-sense-apache-2-patent-license
    
    https://tldrlegal.com/license/apache-license-2.0-%28apache-2.0%29
    
    https://www.gnu.org/licenses/license-recommendations.html

--- WiFiClient.ino
+++ WiFiClient.ino
@@ -5,6 +5,14 @@
  *  
  *  Start with copying settings_default.h to settings.h and add your ssid, password and api key.
  *
+ *  Copyright 2020 Samuel Skånberg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  */
 
 #include <math.h>
